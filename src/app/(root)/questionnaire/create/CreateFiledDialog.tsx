7647df9964e8a3d5b7f99b4afc6311e551671289
Gocha <gochazautashvili2017@gmail.com>
2024-10-06 16:10:17 +0400

    1.1

--- src/app/(root)/questionnaire/create/CreateFiledDialog.tsx
+++ src/app/(root)/questionnaire/create/CreateFiledDialog.tsx
@@ -33,21 +33,19 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { FormDataType, GeneratedFormType } from "@/types";
 import { create_field } from "./actions";
 import { toast } from "@/hooks/use-toast";
-import { parseJson } from "@/lib/utils";
 
 interface Props {
   type: "edit" | "create";
-  content: string | undefined;
+  formData: GeneratedFormType;
   field?: FormDataType;
   onEdit?: () => void;
 }
 
-const CreateFiledDialog = ({ type, field, content, onEdit }: Props) => {
+const CreateFiledDialog = ({ type, field, formData, onEdit }: Props) => {
   const [open, setOpen] = useState(false);
   const [selectedType, setSelectedType] = useState(field?.fieldType || "");
   const [isPending, startGenerate] = useTransition();
 
-  const formData: GeneratedFormType = parseJson(content);
   const options = field?.options?.flatMap((item) => item.value).join(",");
 
   const form = useForm<TFiledSchema>({
--- src/app/(root)/questionnaire/create/Generator.tsx
+++ src/app/(root)/questionnaire/create/Generator.tsx
@@ -6,12 +6,16 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { Form } from "@prisma/client";
+import { GeneratedFormType } from "@/types";
+import { parseJson } from "@/lib/utils";
 
 interface Props {
   form: Form | null;
 }
 
 const Generator = ({ form }: Props) => {
+  const formData: GeneratedFormType = parseJson(form?.content);
+
   return (
     <section className="py-3 fixed bottom-0 left-0 right-0 border-t-2 border-green-500 bg-white/50 backdrop-blur-sm">
       <div className="flex items-center gap-4 w-full max-w-screen-xl mx-auto px-3">
@@ -21,7 +25,7 @@ const Generator = ({ form }: Props) => {
           </Link>
         </Button>
         <AIGeneratorDialog prompt={form?.prompt} />
-        <CreateFiledDialog type="create" content={form?.content} />
+        <CreateFiledDialog type="create" formData={formData} />
         {form && <FormDeleteDialog />}
       </div>
     </section>
